d725082f67ed4653716d3329057570b6a14a901f
yasa515 <168458389+yasa515@users.noreply.github.com>
2024-05-05 13:41:18 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+// Create web server
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, world!');
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Server is running on http://localhost:3000');
+});
